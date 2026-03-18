89324854d2bb95869297694ad1d8182c39ac1587
Jeronimo Acosta  Acevedo <jeronimo.acosta@globant.com>
2026-03-18 09:19:34 -0500

    Add initial files

---
+++ src/test/java/org/example/tests/BaseTest.java
@@ -0,0 +1,29 @@
+package org.example.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public abstract class BaseTest {
+    private WebDriver driver;
+    private static final String URL = "https://www.saucedemo.com/";
+
+    @BeforeMethod
+    public void setup(){
+        //Set up the Selenium WebDriver.
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(URL);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            // Close all browser windows and safely end the session
+            driver.quit();
+        }
+    }
+
+
+}
