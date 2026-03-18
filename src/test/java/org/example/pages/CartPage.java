1e049f79837013d59c99a372c6a16918de530ca4
Jeronimo Acosta  Acevedo <jeronimo.acosta@globant.com>
2026-03-18 10:48:31 -0500

    Add CartPage class along with its web elements and method.

---
+++ src/test/java/org/example/pages/CartPage.java
@@ -0,0 +1,20 @@
+package org.example.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class CartPage extends BasePage{
+    @FindBy(id = "checkout")
+    private WebElement checkoutButton;
+
+
+    public CartPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void checkout(){
+        this.checkoutButton.click();
+    }
+
+}
